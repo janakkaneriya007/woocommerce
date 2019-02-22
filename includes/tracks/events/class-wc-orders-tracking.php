f27079c9c73a8224839089e16a81c456f89fc4a5
Paul Sealock <psealock@gmail.com>
2019-02-22 13:27:16 +1300

    sanitize

--- includes/tracks/events/class-wc-orders-tracking.php
+++ includes/tracks/events/class-wc-orders-tracking.php
@@ -23,27 +23,25 @@ class WC_Orders_Tracking {
 	 * Send a Tracks event when the Orders page is viewed.
 	 */
 	public static function track_orders_view() {
-		WC_Tracks::record_event( 'orders_view' );
+		if ( isset( $_GET['post_type'] ) && 'shop_order' === sanitize_text_field( wp_unslash( $_GET['post_type'] ) ) ) {
+			WC_Tracks::record_event( 'orders_view' );
+		}
 	}
 
 	/**
 	 * Send a Tracks event when an order status is changed.
 	 *
 	 * @param int    $id Order id.
-	 * @param string $new_status WooCommerce order status.
+	 * @param string $previous_status the old WooCommerce order status.
+	 * @param string $next_status the new WooCommerce order status.
 	 */
 	public static function track_order_status_change( $id, $previous_status, $next_status ) {
 		$properties = array(
-			'order_id'   => $id,
-			'next_status' => $next_status,
+			'order_id'        => $id,
+			'next_status'     => $next_status,
 			'previous_status' => $previous_status,
 		);
 
 		WC_Tracks::record_event( 'orders_edit_status_change', $properties );
 	}
 }
-
-add_filter( 'http_request_args', function( $r, $url ) {
-	error_log( $r['method'] . ' ' . $url );
-	return $r;
-},10, 2 );
